8c1a98276e1d5ba90d96e32fb1ceb00b0fe2fe7c
StéphaneCash <kikonistephane@gmail.com>
2021-10-31 23:22:27 +0100

    s

--- src/App.js
+++ src/App.js
@@ -4,13 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Project Energie Solaire en cours de construction...
-        </p>
-
-      </header>
+      App
     </div>
   );
 }
